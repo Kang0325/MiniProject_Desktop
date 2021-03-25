4a3f96aea6d5ee7af9281a9961b2615b0087acc7
Kang0325 <noworry1@naver.com>
2021-03-25 17:25:29 +0900

    210325 수업완료 커밋

---
+++ WpfSMSApp/WpfSMSApp/Helper/Commons.cs
@@ -0,0 +1,18 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfSMSApp.Model;
+
+namespace WpfSMSApp.Helper
+{
+    public class Commons
+    {
+        // NLog 정적 인스턴스 생성
+        public static readonly Logger LOGGER = LogManager.GetCurrentClassLogger();
+
+        public static User LOGINED_USER;
+    }
+}
