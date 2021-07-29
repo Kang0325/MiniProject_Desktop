00317fa0cbac9857e4ffaaf4ea34cb39d916ba55
Kang0325 <noworry1@naver.com>
2021-07-29 10:15:39 +0900

    210729 무비파인더

--- WpfMiniProject/NaverMovieFinderApp/MainWindow.xaml.cs
+++ WpfMiniProject/NaverMovieFinderApp/MainWindow.xaml.cs
@@ -6,19 +6,11 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Data.Entity;
-using MahApps.Metro.Controls.Dialogs;
 
 namespace NaverMovieFinderApp
 {
--- WpfMiniProject/NaverMovieFinderApp/TrailerWindow.xaml.cs
+++ WpfMiniProject/NaverMovieFinderApp/TrailerWindow.xaml.cs
@@ -5,19 +5,10 @@ using NaverMovieFinderApp.Helper;
 using NaverMovieFinderApp.Model;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace NaverMovieFinderApp
 {
